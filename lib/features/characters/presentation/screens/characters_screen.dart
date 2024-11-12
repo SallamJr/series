157fb39f7812b0c4b557c592925f56a3231be3eb
Ahmed Sallam <as1715540@gmail.com>
2024-11-12 13:27:14 +0200

    Features Structure

---
+++ lib/features/characters/presentation/screens/characters_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+part '../widgets/build_characters_screen_body.dart';
+
+class CharactersScreen extends StatelessWidget {
+  const CharactersScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const _BuildCharactersScreenBody();
+  }
+}
---
+++ lib/features/characters/presentation/widgets/build_characters_screen_body.dart
@@ -0,0 +1,10 @@
+part of '../screens/characters_screen.dart';
+
+class _BuildCharactersScreenBody extends StatelessWidget {
+  const _BuildCharactersScreenBody();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
